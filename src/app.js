179f5ccf430aa24b77da24e904256bcdfce7c977
amaliauka <aleksmalyavko@gmail.com>
2019-05-22 18:42:11 +0300

    added cors lib

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
@@ -11,12 +12,13 @@ process.env.CONNECTION_STRING = "mongodb+srv://aleksmal:qwerty1234@cluster0-olbq
 
 mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, POST, DELETE, PATCH');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, POST, DELETE, PATCH');
+//   next();
+// });
+app.use(cors());
 app.options('*', (req, res) => res.end());
 
 app.use(bodyParser.json());
