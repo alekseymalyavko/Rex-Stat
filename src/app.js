6a25b9462d977877792e11e76614255d00d9b4b1
amaliauka <aleksmalyavko@gmail.com>
2019-05-23 18:03:26 +0300

    updated

--- src/app.js
+++ src/app.js
@@ -8,16 +8,10 @@ import cors from 'cors';
 
 const app = express();
 
-// process.env.CONNECTION_STRING = "mongodb+srv://aleksmal:qwerty1234@cluster0-ezzur.mongodb.net/test?retryWrites=true"
+const urlDb = "mongodb+srv://aleksmal:qwerty1234@cluster0-ezzur.mongodb.net/test?retryWrites=true"
 
-mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
+mongoose.connect(process.env.CONNECTION_STRING || urlDb, { useNewUrlParser: true });
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, POST, DELETE, PATCH');
-//   next();
-// });
 app.use(cors());
 app.options('*', (req, res) => res.end());
 
@@ -26,6 +20,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
-const server = app.listen(process.env.PORT || 3333, () => {
+app.get('/', function(req, res) {
+  res.send('<h1>Server is running...</h1>');
+});
+
+const server = app.listen(process.env.PORT || 3001, () => {
   console.log(`Server listening on port ${server.address().port}`);
 });
